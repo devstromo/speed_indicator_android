9ebb39712b0ce5f64e9ac27ed7645dad438c9208
alemoles <alemolerio@gmail.com>
2024-03-26 21:29:37 -0300

    Speed Indicator Android Kotlin | code refactor

--- app/src/main/java/com/devstromo/speedindicatorandroid/ui/presentation/SpeedIndicator.kt
+++ app/src/main/java/com/devstromo/speedindicatorandroid/ui/presentation/SpeedIndicator.kt
@@ -2,17 +2,8 @@ package com.devstromo.speedindicatorandroid.ui.presentation
 
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Composable
 fun SpeedIndicator() {
