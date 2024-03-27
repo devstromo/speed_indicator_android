e2ea38718f62556c808cdcaaab785db81561564d
alemoles <alemolerio@gmail.com>
2024-03-26 21:48:27 -0300

    Speed Indicator Android Kotlin | add sample slider to control the needle

--- app/src/main/java/com/devstromo/speedindicatorandroid/ui/presentation/SpeedIndicator.kt
+++ app/src/main/java/com/devstromo/speedindicatorandroid/ui/presentation/SpeedIndicator.kt
@@ -2,14 +2,42 @@ package com.devstromo.speedindicatorandroid.ui.presentation
 
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SpeedIndicator() {
+    var sliderValue by remember {
+        mutableFloatStateOf(0f)
+    }
     BoxWithConstraints(
         modifier = Modifier.fillMaxSize()
     ) {
         ExternalArc()
+        Text(
+            text = sliderValue.toString(),
+            modifier = Modifier.align(Alignment.Center)
+        )
+        Slider(
+            modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.BottomCenter),
+            value = sliderValue,
+            onValueChange = {
+                sliderValue = it
+            },
+            steps = 220,
+            valueRange = 0f..220f,
+            enabled = true
+        )
     }
 }
