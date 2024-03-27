baf42162a868576a61bc1a772ea3f76c16411ed1
alemoles <alemolerio@gmail.com>
2024-03-26 21:30:10 -0300

    Speed Indicator Android Kotlin | code refactor

--- app/src/main/java/com/devstromo/speedindicatorandroid/MainActivity.kt
+++ app/src/main/java/com/devstromo/speedindicatorandroid/MainActivity.kt
@@ -3,11 +3,9 @@ package com.devstromo.speedindicatorandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
