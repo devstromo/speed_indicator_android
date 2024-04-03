7415f3bdd7b812240780af043d2602ed59f2993c
alemoles <alemolerio@gmail.com>
2024-04-02 23:17:05 -0300

    Speed Indicator Android Kotlin | update calculation values

--- app/src/main/java/com/devstromo/speedindicatorandroid/ui/presentation/SpeedIndicator.kt
+++ app/src/main/java/com/devstromo/speedindicatorandroid/ui/presentation/SpeedIndicator.kt
@@ -1,12 +1,9 @@
 package com.devstromo.speedindicatorandroid.ui.presentation
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
@@ -36,7 +32,11 @@ fun SpeedIndicator() {
                 .padding(start = 100.dp)
                 .graphicsLayer {
                     this.transformOrigin = TransformOrigin(0f, 0f)
-                    this.rotationZ = sliderValue
+                    this.rotationZ = mapValueToRange(
+                        originalValue = sliderValue,
+                        originalMin = 0f, originalMax = 240f,
+                        newMin = 140f, newMax = 400f
+                    )
                 }) {
             Needle()
         }
@@ -56,4 +56,12 @@ fun SpeedIndicator() {
             enabled = true
         )
     }
+}
+
+fun mapValueToRange(
+    originalValue: Float,
+    originalMin: Float, originalMax: Float,
+    newMin: Float, newMax: Float
+): Float {
+    return (originalValue - originalMin) / (originalMax - originalMin) * (newMax - newMin) + newMin
 }
